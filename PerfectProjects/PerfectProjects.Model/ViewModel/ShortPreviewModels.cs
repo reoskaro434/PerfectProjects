8c8b5397f72cddc51c514dfa37cf658bed1e78e2
reoskaro434 <reoskaro434@gmail.com>
2021-03-12 23:29:16 +0100

    show short desc project on main page

---
+++ PerfectProjects/PerfectProjects.Model/ViewModel/ShortPreviewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectProjects.Model.ViewModel
+{
+    public class ShortPreviewModels
+    {
+        public IEnumerable<ShortPreviewModel> shortPreviewModels { get; set; }
+    }
+}
--- PerfectProjects/PerfectProjects/Areas/User/Controllers/HomeController.cs
+++ PerfectProjects/PerfectProjects/Areas/User/Controllers/HomeController.cs
@@ -1,27 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PerfectProjects.DataAccess.RepositoryPattern;
+using PerfectProjects.Model;
 using PerfectProjects.Model.ViewModel;
+using PerfectProjects.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace PerfectProjects.Controllers
-{       
+{
     [Area("User")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(GetAccessibleShortPreviews());
         }
 
         public IActionResult Privacy()
@@ -33,6 +38,39 @@ namespace PerfectProjects.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ShortPreviewModels GetAccessibleShortPreviews()
+        {
+            //short description
+            IEnumerable<ShortDescription> shortDescriptions = _unitOfWork.ShortDescriptions.GetAll();
+
+            //users info
+            IEnumerable<ApplicationUser> applicationUsers = _unitOfWork.ApplicationUsers.GetAll();
+
+            List<ShortPreviewModel> previewModels = new List<ShortPreviewModel>();
+            
+            foreach (ShortDescription element in shortDescriptions)
+            {
+               var user = applicationUsers.Where(x => x.Id == element.UserId).FirstOrDefault();//user's id is unique
+                if(user != null)
+                {
+                    var shortPrevModel = new ShortPreviewModel
+                    {
+                        ShortDescription = element,
+                        NickName = user.NickName,
+                        ImageString = ImageManager.ConvertToString(element.Image),
+                    };
+
+                    previewModels.Add(shortPrevModel);
+                }
+                else return null; //smth wrong, returns null
+            }
+
+            var shortPrevievModels = new ShortPreviewModels();
+            shortPrevievModels.shortPreviewModels = previewModels;
+
+            return shortPrevievModels;
         }
     }
 }

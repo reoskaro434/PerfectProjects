795dcb6c98731adab364456a030993e305c12e61
reoskaro434 <reoskaro434@gmail.com>
2021-03-13 23:05:03 +0100

    HomePagePicFix has been created

--- PerfectProjects/PerfectProjects/wwwroot/js/HomePagePicFix.js
+++ PerfectProjects/PerfectProjects/wwwroot/js/HomePagePicFix.js
@@ -1,1 +1,16 @@
-﻿
+﻿window.onresize = resizeImg;
+window.onload = resizeImg;
+
+function resizeImg() {
+
+    var ctr = document.getElementById('counter');
+    var counterNumber = Number(ctr.textContent);
+    for (var i = 1; i <= counterNumber;i++)
+    {
+        console.log('output' + i);
+        var image = document.getElementById('output' + i);
+        if (image) {
+            image.width = document.getElementById('textArea').offsetWidth;
+        }
+    }
+}

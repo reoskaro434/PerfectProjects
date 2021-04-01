6b51bece98bfbd936ca534481eb241c5bb7f57d5
reoskaro434 <reoskaro434@gmail.com>
2021-04-01 20:55:15 +0200

    create project controller, view

---
+++ PerfectProjects/PerfectProjects/Areas/User/Controllers/CreateProjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectProjects.Areas.User.Controllers
+{
+    [Area("User")]
+    public class CreateProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

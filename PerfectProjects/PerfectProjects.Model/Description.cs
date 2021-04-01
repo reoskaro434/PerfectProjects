fd7f38f46d4f52e954fc8249ff45404600701805
reoskaro434 <reoskaro434@gmail.com>
2021-04-01 21:53:16 +0200

    description model

---
+++ PerfectProjects/PerfectProjects.Model/Description.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectProjects.Model
+{
+    public class Description
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public IEnumerable<string> LongDescriptions { get; set; }
+
+        [MaxLength(83886080)]
+        public IEnumerable<byte[]> Images { get; set; }
+
+        public ShortDescription ShortDescription { get; set; }
+    }
+}
+
